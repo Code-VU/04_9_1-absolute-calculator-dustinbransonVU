819ebae24a3707f87b912c5ff25bffee86e52f44
Dustin Branson <Dustin.Branson@vu.com>
2024-03-13 17:18:19 -0500

    dunno

--- test_Program.py
+++ test_Program.py
@@ -1,11 +1,14 @@
-in_num = input("Enter a number: ")
-
 def ab_dif(in_num):
-    diff = abs(in_num-21)
+    in_num = int(in_num)  # Convert the input to an integer
+    diff = abs(in_num - 21)
     if in_num > 21:
         return diff * 2
     else:
         return diff
+
+in_num = input("Enter a number: ")
+result = ab_dif(in_num)
+print("Absolute difference:", result)
     
     
     
